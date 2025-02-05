35ad082d63ca9ef433811030a0be7495cf24acc9
a-estevam <estevam.adesign@gmail.com>
2025-02-04 21:29:07 -0300

    cartContext

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,28 @@ import Header from './Components/Header/Header'
 import RoutesComponents from './Routes/RoutesComponents'
 import Footer from './Components/Footer/Footer'
 import { CartProvider } from './context/CartProvider'
+// import { collection, doc, getDoc, getDocs, getFirestore } from 'firebase/firestore'
+// import { useEffect, useState } from 'react'
+
+
 
 
 function App() {
+
+  // const [products, setProducts] = useState([])
+
+  // useEffect(() =>{
+  //   const db = getFirestore();
+  //   const ItemsCollection = collection(db, "items", "uyXLON8VSjmxLCopp7PC");
+  //   getDocs(ItemsCollection).then((snapshot) =>{
+  //     if (snapshot.size === 0) {
+  //       console.log("No result")
+  //     }
+  //     setProducts(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data()})))
+  //   })
+    
+
+  // }, [])
   
 
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,28 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDdEj1M9C8jpZ1azWMb9Ty3TiRsBI-HpFs",
+  authDomain: "ppratas-c1f89.firebaseapp.com",
+  projectId: "ppratas-c1f89",
+  storageBucket: "ppratas-c1f89.firebasestorage.app",
+  messagingSenderId: "960319144109",
+  appId: "1:960319144109:web:2f4de98c3ebdef01000624",
+  measurementId: "G-QLX60XXT1Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
